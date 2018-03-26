f974be57cfadf942071c7b9be398b787b8d68659
avalonstrel <linhy960303@gmail.com>
2018-03-26 14:55:22 +0800

    a

--- yours.c
+++ yours.c
@@ -397,7 +397,7 @@ int test(int block_size, int n){
 int main(int argc, char *argv[]) {
     for(int i = 16; i < 256; i+=16){
         for(int j = 1000; j < 1099; j += 5){
-            printf("block size: %d, matrix size %d",i, j);
+            printf("block size: %d, matrix size %d\n",i, j);
             test(i, j);
             printf("\n");
         }
