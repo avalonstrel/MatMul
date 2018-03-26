21274112f14cbbe69cb1c7dd0b702806b5f72bc0
avalonstrel <linhy960303@gmail.com>
2018-03-26 14:05:16 +0800

    a

--- yours.c
+++ yours.c
@@ -80,7 +80,7 @@ double *YoursBlocked(int n, double *A, double *B, int BLOCK_SIZE) {
     if(n < BLOCK_SIZE){
         BLOCK_SIZE = n;
     }
-        unsigned int i_block_size = BLOCK_SIZE;
+    unsigned int i_block_size = BLOCK_SIZE;
     unsigned int j_block_size = BLOCK_SIZE;
     unsigned int k_block_size = BLOCK_SIZE;
     double sum = 0.0;
@@ -496,12 +496,12 @@ int main(int argc, char *argv[]) {
     double *Y;
     Y = (double *) malloc(n * n * sizeof(double));
     Y = generate(n);
-    Y = Naive(n,A,B);
+    // Y = Naive(n,A,B);
 
-    if (check(Y, A, B, n))
-        printf("N TRUE%d\n", 1);
-    else
-        printf("N FALSE%d\n", 0);
+    // if (check(Y, A, B, n))
+    //     printf("N TRUE%d\n", 1);
+    // else
+    //     printf("N FALSE%d\n", 0);
 
     Y = YoursBlocked(n, A, B, BLOCK_SIZE);
 
